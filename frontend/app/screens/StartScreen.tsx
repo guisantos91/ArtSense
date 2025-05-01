a7e6713e2b861823c79c836c443c9734642ba9dc
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2025-05-01 16:21:20 +0100

    startpages started

--- frontend/app/screens/StartScreen.tsx
+++ frontend/app/screens/StartScreen.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
 
 const StartScreen = () => {
   return (
