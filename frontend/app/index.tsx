fb8615522d7256dab9cc1ce04d482fd70990c5e7
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2025-05-01 15:37:16 +0100

    fonts

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,6 +1,19 @@
 import { Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function Index() {
+  const [fontsLoaded] = useFonts({
+    CormorantGaramond: require("../assets/fonts/CormorantGaramond-VariableFont_wght.ttf"),
+    "CormorantGaramond-Italic": require("../assets/fonts/CormorantGaramond-Italic-VariableFont_wght.ttf"),
+    EBGaramond: require("../assets/fonts/EBGaramond-VariableFont_wght.ttf"),
+    "EBGaramond-Italic": require("../assets/fonts/EBGaramond-Italic-VariableFont_wght.ttf"),
+    Inter: require("../assets/fonts/Inter-VariableFont_opsz,wght.ttf"),
+    "Inter-Italic": require("../assets/fonts/Inter-Italic-VariableFont_opsz,wght.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View
       style={{
@@ -9,7 +22,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="font-cormorant">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#202020",
+      secondary: "#303030",
+      tertiary: "#707070",
+      quaternary: "#CFCFCF",
+      quinary: "#F0EACE",
+      senary: "#EEEEEE",
+      septenary: "#323232",
+      octonary: "#252525",
+      nonary: "#3A3A3A",
+      decenary: "#454545",
+    },
+    fontFamily: {
+      cormorant: ["CormorantGaramond"],
+      cormorantItalic: ["CormorantGaramond-Italic"],
+      ebgaramond: ["EBGaramond"],
+      ebgaramondItalic: ["EBGaramond-Italic"],
+      inter: ["Inter"],
+      interItalic: ["Inter-Italic"],
+    },
   },
   plugins: [],
-}
+};
