726a7d03557a3b54998e510f804cad2869481c9b
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2025-05-02 17:37:18 +0100

    new code updates

--- frontend/app/components/ArtifactBottomSheet.tsx
+++ frontend/app/components/ArtifactBottomSheet.tsx
@@ -117,6 +117,71 @@ const ArtifactBottomSheet = ({
                   <AntDesign name="arrowleft" size={34} color="#D9D8DE" />
                 </TouchableOpacity>
               </View>
+
+              <View className="absolute mt-[55%] flex-1 w-full self-center">
+                <Text className="text-white text-5xl text-[40px] font-ebgaramond leading-none">
+                  Der Wanderer über dem Nebelmeer
+                </Text>
+                <Text className="text-white text-base font-inter mt-1">
+                  1818, Germany
+                </Text>
+
+                <View className="flex-row justify-between">
+                  <View className="space-y-4 w-[50%] mt-6">
+                    <View>
+                      <Text className="text-white font-bold text-xl mt-4">
+                        Dimensions
+                      </Text>
+                      <Text className="text-white text-base">148 × 178 cm</Text>
+                    </View>
+                    <View>
+                      <Text className="text-white font-bold text-xl mt-4">
+                        Material
+                      </Text>
+                      <Text className="text-white text-base">
+                        Oil on Canvas
+                      </Text>
+                    </View>
+                    <View>
+                      <Text className="text-white font-bold text-xl mt-4">
+                        Year
+                      </Text>
+                      <Text className="text-white text-base">1818</Text>
+                    </View>
+                  </View>
+
+                  <View className="w-[40%] items-start space-y-2">
+                    <Image
+                      source={require("../../assets/images/imgs/exhibition.png")}
+                      className="w-full h-40 rounded-md mb-2 self-end"
+                    />
+                    <Text className="text-white font-ebgaramond font-semibold text-lg">
+                      Caspar David Friedrich
+                    </Text>
+                    <Text className="text-white text-xs leading-snug mt-4">
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standard dummy text ever since the 1500s, when an unknown
+                      printer
+                    </Text>
+                  </View>
+                </View>
+                <View className="items-center flex-1">
+                  <Animated.View
+                    style={{ transform: [{ translateY: arrowAnimation }] }}
+                  >
+                    <AntDesign name="arrowdown" size={24} color="white" />
+                  </Animated.View>
+
+                  <TouchableOpacity>
+                    <View className="bg-senary rounded-2xl px-4 py-3 mt-2 self-center">
+                      <Text className="text-center text-primary">
+                        Ask what you want to know
+                      </Text>
+                    </View>
+                  </TouchableOpacity>
+                </View>
+              </View>
             </>
           )}
 
@@ -138,70 +203,6 @@ const ArtifactBottomSheet = ({
             </View>
           )}
         </ImageBackground>
-        {sheetIndex === 1 && (
-          <View className="absolute top-[22%] flex-1 px-8 w-full">
-            <Text className="text-white text-5xl text-[40px] font-ebgaramond leading-none">
-              Der Wanderer über dem Nebelmeer
-            </Text>
-            <Text className="text-white text-base font-inter mt-1">
-              1818, Germany
-            </Text>
-
-            <View className="flex-row justify-between">
-              <View className="space-y-4 w-[50%] mt-6">
-                <View>
-                  <Text className="text-white font-bold text-xl mt-4">
-                    Dimensions
-                  </Text>
-                  <Text className="text-white text-base">148 × 178 cm</Text>
-                </View>
-                <View>
-                  <Text className="text-white font-bold text-xl mt-4">
-                    Material
-                  </Text>
-                  <Text className="text-white text-base">Oil on Canvas</Text>
-                </View>
-                <View>
-                  <Text className="text-white font-bold text-xl mt-4">
-                    Year
-                  </Text>
-                  <Text className="text-white text-base">1818</Text>
-                </View>
-              </View>
-
-              <View className="w-[40%] items-start space-y-2">
-                <Image
-                  source={require("../../assets/images/imgs/exhibition.png")}
-                  className="w-full h-40 rounded-md mb-2 self-end"
-                />
-                <Text className="text-white font-ebgaramond font-semibold text-lg">
-                  Caspar David Friedrich
-                </Text>
-                <Text className="text-white text-xs leading-snug mt-4">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer
-                </Text>
-              </View>
-            </View>
-            <View className="items-center flex-1 mt-4">
-              <Animated.View
-                style={{ transform: [{ translateY: arrowAnimation }] }}
-              >
-                <AntDesign name="arrowdown" size={24} color="white" />
-              </Animated.View>
-
-              <TouchableOpacity>
-                <View className="bg-senary rounded-2xl px-4 py-3 mt-4 self-center">
-                  <Text className="text-center text-primary">
-                    Ask what you want to know
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            </View>
-          </View>
-        )}
       </BottomSheetView>
     </BottomSheet>
   );
